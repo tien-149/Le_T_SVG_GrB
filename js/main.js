ee03dd36be50df21669054b3aa5a7bd7926ad85b
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-01-22 12:45:56 -0500

    This is the final JS practice on week 3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-(() => { 
- //self -invoking anonymous function or Lambda}
- //
- //
- //var => mean create a variable
- //whatever comes after the = is what the variable 
- var images = document.querySelectorAll('img');
+	// What do we want to work with
+	(() => {
 
- // document is always a webpage
- // after the dot is always a method 
 
- console.log(images);
-})();
+		// this is a 1 to 1 connection (selects one element)
+		var badge = document.querySelector('#badge');
+
+		// This is a one to many selection (gets a collection of elements)
+		var allTheBadges = document.querySelectorAll('.isabadge');
+				// what do we want it to do
+		function logMyId() {
+			// log some stuff to the dev tools
+			console.log(this.id);
+			//debugger;
+		}
+		// How tdo we want to do that?
+		// 1 to 1 event handling
+		badge.addEventListener("click", logMyId);
+
+
+		allTheBadges.forEach(currentbadge => currentbadge.addEventListener('click', logMyId));
+
+	})();
